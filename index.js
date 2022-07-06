4d99b6815f4da03cfd4ce92d71179338dccc512d
darshitmakasan <darshit@catapulthq.com>
2022-07-06 13:15:21 +0530

    update changes

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
-const server = app.listen(8080, () => {
-    console.log('Server Started..');
+const server = app.listen(4000, () => {
+  console.log("Server Started..");
 });
 
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const io = socket(server);
 
-io.on('connection', (socket) => {
-    console.log('socket connected..', socket.id);
+io.on("connection", (socket) => {
+  console.log("socket connected..", socket.id);
 
-    socket.on('chat', (data) => {
-        io.sockets.emit('chat', data);
-    });
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
 
-    socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data);
-    });
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
 });
